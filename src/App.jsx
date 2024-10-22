155cd0e08ac7f641892d0f5c723e47984909ea4d
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-10-22 16:38:55 -0400

    adds basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/scss/styles.scss';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// Import our custom CSS
+import '../src/assets/scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
